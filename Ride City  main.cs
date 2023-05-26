0ab3531258648ddea1b99a0a89b7d3fad83bf611
DaryllAmurao <129594498+DaryllAmurao@users.noreply.github.com>
2023-05-26 20:37:18 +0800

    Put listing and method.

--- Ride City  main.cs
+++ Ride City  main.cs
@@ -4,77 +4,33 @@ namespace BigBikes
 {
 class Program {
   static void Main() {
-    Console.WriteLine("Welcome to Ride City!");
+    Console.WriteLine("**Welcome to Ride City!**");
     Console.ReadLine();
     Console.WriteLine("Check out our bikes!");
     Console.WriteLine("*****************");
-    
-    //Name of Bigbikes
-    Console.WriteLine("1. Kawasaki Z1000R ");
-    Console.WriteLine("2. 2023 Kawasaki Z H2");
-    Console.WriteLine("3. Ducati Panigale V4");
-    Console.WriteLine("4. 2023 Honda CB650r");
-    Console.WriteLine("5. 2023 Suzuki Hayabusa");
-    
-    Console.WriteLine("*****************");
-    Console.WriteLine("Kindly enter the number of the chosen bike: ");
-    string userInput = Console.ReadLine();
-    
+
+    //call
+    listOfBikes();
     switch (userInput)
     {
         case "1":
-            Console.WriteLine("Kawasaki Z1000R:");
-            Console.WriteLine("   - Liquid Cooled");
-            Console.WriteLine("   - In-line Four");
-            Console.WriteLine("   - 1043cc");
-            Console.WriteLine("   - 142 HP @ 10,000 rpm");
-            Console.WriteLine("   - 6-Speed manual transmission");
-            Console.WriteLine("   - 17L Fuel Capacity");
-            Console.WriteLine("   - SRP: Php 710,000 ");
+            caseOne();
             break;
          
         case "2":
-            Console.WriteLine("2023 Kawasaki Z H2");
-            Console.WriteLine("   - Liquid Cooled");
-            Console.WriteLine("   - In-line Four");
-            Console.WriteLine("   - 998cc");
-            Console.WriteLine("   - 197.4 HP @ 11,000 rpm");
-            Console.WriteLine("   - 6-Speed manual transmission");
-            Console.WriteLine("   - 19L Fuel Capacity");
-            Console.WriteLine("   - SRP: Php 920,000 ");
-            break;
+             caseTwo();
+;            break;
             
         case "3":
-            Console.WriteLine("Ducati Panigale V4");
-            Console.WriteLine("   - Liquid Cooled");
-            Console.WriteLine("   - V4 Engine");
-            Console.WriteLine("   - 1103cc");
-            Console.WriteLine("   - 215.5 HP @ 13,000 rpm");
-            Console.WriteLine("   - 6-Speed manual transmission");
-            Console.WriteLine("   - 16L Fuel Capacity");
-            Console.WriteLine("   - SRP: Php 1,990,000 ");
-            break;   
+             caseThree();
+             break;   
             
         case "4":
-            Console.WriteLine("2023 Honda CB650r");
-            Console.WriteLine("   - Liquid Cooled");
-            Console.WriteLine("   - In-line Four");
-            Console.WriteLine("   - 649cc");
-            Console.WriteLine("   - 94HP @ 12,000 rpm");
-            Console.WriteLine("   - 6-Speed manual transmission");
-            Console.WriteLine("   - 15.4L Fuel Capacity");
-            Console.WriteLine("   - SRP: Php 519,000 ");
+            caseFour();
             break;
         
         case "5":
-            Console.WriteLine("2023 Suzuki Hayabusa");
-            Console.WriteLine("   - Liquid Cooled");
-            Console.WriteLine("   - In-line Four");
-            Console.WriteLine("   - 1340cc");
-            Console.WriteLine("   - 187HP @ 9,700 rpm");
-            Console.WriteLine("   - 6-Speed manual transmission");
-            Console.WriteLine("   - 20L Fuel Capacity");
-            Console.WriteLine("   - SRP: Php 1,199,000 ");
+            caseFive();
             break;
     }
 
@@ -88,12 +44,98 @@ class Program {
     {
         Console.WriteLine("Please write your number.");
         Console.ReadLine();
+        Console.WriteLine("Enter the date for your test ride(DD-MM-YYYY):");
+        Console.ReadLine();
+        Console.WriteLine("We appreciate it, our staff will reach you within 24 hours.");
+		
     }   
     
     if(userInputOne == "B")
     {    
-        Console.WriteLine("Alright, thanks!");
+        Console.WriteLine();
+        Console.WriteLine("Alright!");
     }
-   }
+  }
+
+    static void listOfBikes()
+    {
+            var listOfBikes = new list<string>()
+            {
+            "1. Kawasaki Z1000R ","2. 2023 Kawasaki Z H2", "3. Ducati Panigale V4",
+            "4. 2023 Honda CB650r", "5. 2023 Suzuki Hayabusa"
+            };
+            foreach (string i in listOfBikes) 
+            { 
+                Console.WriteLine( i ); 
+            }
+            Console.WriteLine("*****************");
+            Console.WriteLine("Kindly enter the number of the chosen bike: ");
+            string userInput = Console.ReadLine();
+    }
+    static void caseOne()
+        {
+            var caseOne = new list<string>()
+            {
+            "Kawasaki Z1000R:","   - Liquid Cooled", "   - In-line Four","   - 1043cc",
+            "   - 142 HP @ 10,000 rpm", "   - 6-Speed manual transmission", "   - 17L Fuel Capacity",
+            "   - SRP: Php 710,000 "
+            }
+            foreach (string i in caseOne)
+            { 
+             Console.Write( i ); 
+            }
+        }
+    static void caseTwo()
+    {
+            var caseTwo = new list<string>()
+            {
+            "2023 Kawasaki Z H2", "   - Liquid Cooled", "   - In-line Four","   - 998cc",
+            "   - 197.4 HP @ 11,000 rpm","   - 6-Speed manual transmission", "   - 19L Fuel Capacity",
+            "   - SRP: Php 920,000 "
+            };
+            foreach (string i in caseTwo)
+            {
+                Console.Write(i);
+            }
+    }
+    static void caseThree()
+    {
+            var caseThree = new list<string>()
+            { "Ducati Panigale V4", "   - Liquid Cooled", "   - V4 Engine", "   - 1103cc",
+            "   - 215.5 HP @ 13,000 rpm", "   - 6-Speed manual transmission", "   - 16L Fuel Capacity",
+            "   - SRP: Php 1,990,000 "
+            };
+            foreach (string i in caseThree)
+            {
+                Console.Write(i);
+            }
+
+    }
+    static void caseFour() 
+    { 
+            var caseFour =  new list<string>()
+            { "2023 Honda CB650r", "   - Liquid Cooled", "   - In-line Four", "   - 649cc",
+            "   - 94HP @ 12,000 rpm", "   - 6-Speed manual transmission", "   - 15.4L Fuel Capacity",
+            "   - SRP: Php 519,000 "
+            }
+            foreach (string i in caseFour)
+            {
+                Console.WriteLine(i);   
+            }
+ 
+    }
+    static void caseFive()
+        { 
+            var caseFive = new list<string>()
+            {
+             "2023 Suzuki Hayabusa", "   - Liquid Cooled", "   - In-line Four", "   - 1340cc", "   - 187HP @ 9,700 rpm",
+             "   - 6-Speed manual transmission", "   - 20L Fuel Capacity", "   - SRP: Php 1,199,000 "
+            }
+            foreach (string i in caseFive)
+            {
+             Console.Writeline(i);
+            }
+      
+        }
   }
 }
